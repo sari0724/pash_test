e4858487195dbded882a5914ea5bff861b77efc7
sari0724 <926sari@gmail.com>
2022-03-06 08:15:49 +0900

    ブログ投稿の削除処理実装

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -10,6 +10,12 @@
     </head>
     <body>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id='form_delete' method="post">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+            <input type="submit" style="display:inline">
+            <p class='delete'>[<span onclick='return deletePost(this);'>delete</span>]</p>
+        </form>
         <h1 class="title">
             {{ $post->title }}
         </h1>
@@ -22,5 +28,13 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e) {
+                'use strict';
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
